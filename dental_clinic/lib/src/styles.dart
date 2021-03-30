a4ef68d5a32535db21b29d001e40b1ddc440a070
Tuong Nguyen (Alan) <tuong@buuuk.com>
2021-03-30 11:54:43 +0700

    Simple login with firebase

---
+++ dental_clinic/lib/src/styles.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+
+abstract class Styles {
+
+  static const TextStyle productRowItemName = TextStyle(
+    color: Color.fromRGBO(0, 0, 0, 0.8),
+    fontSize: 18,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const TextStyle productRowTotal = TextStyle(
+    color: Color.fromRGBO(0, 0, 0, 0.8),
+    fontSize: 18,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle searchText = TextStyle(
+    color: Color.fromRGBO(0, 0, 0, 1),
+    fontSize: 14,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const TextStyle productRowItemPrice = TextStyle(
+    color: Color(0xFF8E8E93),
+    fontSize: 13,
+    fontWeight: FontWeight.w300,
+  );
+
+  static const Color color_primary = Colors.green;
+
+  static const Color productRowDivider = Color(0xFFD9D9D9);
+
+  static const Color scaffoldBackground = Color(0xfff0f0f0);
+
+  static const Color searchBackground = Color(0xffe0e0e0);
+
+  static const Color searchCursorColor = Color.fromRGBO(0, 122, 255, 1);
+
+  static const Color searchIconColor = Color.fromRGBO(128, 128, 128, 1);
+}
